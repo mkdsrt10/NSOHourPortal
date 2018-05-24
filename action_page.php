10805ad63bc5ca311cf46f8f3a1a8ba57fc5c268
mkdsrt10 <dmayank0@icloud.com>
2018-05-24 09:46:10 +0530

    AttnMonth updated

---
+++ action_page.php
@@ -0,0 +1,6 @@
+<?php
+  if ($_POST["entryNo"]=="2017"&&$_POST["psw"]=="may") {
+    getElementById('session-user').innerHTML.getText = "Summary for $_POST["entryNo"]"
+    location.href='SummaryPage.html'
+  }
+ ?>
